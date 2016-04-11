8759c60497b465f4eef00b532c5344c08adbc6f4
Joskef <tekkenaddict56@gmail.com>
2016-04-11 22:11:01 +0800

    removed appointments table

--- udc_database.sql
+++ udc_database.sql
@@ -18,30 +18,6 @@ USE `udc_database`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `appointment`
---
-
-DROP TABLE IF EXISTS `appointment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `appointment` (
-  `apptno` int(11) NOT NULL AUTO_INCREMENT,
-  `slotno` int(5) NOT NULL,
-  PRIMARY KEY (`apptno`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `appointment`
---
-
-LOCK TABLES `appointment` WRITE;
-/*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
-INSERT INTO `appointment` VALUES (1,2),(2,1);
-/*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `doctors`
 --
 
@@ -104,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-25 16:33:56
+-- Dump completed on 2016-04-11 22:09:41
